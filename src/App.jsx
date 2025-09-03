809f0886033cb8e9b4ef77fc5eb5327ec91f13a7
Sayed Ahemmad <143632368+Sayeed-5@users.noreply.github.com>
2025-09-03 19:34:24 +0530

    add agrimart page

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,21 @@ import Orders from "./pages/Orders"
 import LandingPage from './pages/LandingPage';
 import SigninPage from './pages/SignIn';
 import SellerPage from './pages/Seller';
+import AgriMart from './pages/AgriMart';
+import Header from './components/Header';
 
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<SigninPage />} />
-        <Route path="/orders" element={<Orders />} /> 
-        <Route path="/seller" element={<SellerPage />} /> 
-      </Routes>
+      <>
+        <Header />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<SigninPage />} />
+          <Route path="/orders" element={<Orders />} /> 
+          <Route path="/seller" element={<SellerPage />} /> 
+          <Route path="/agrimart" element={<AgriMart />} /> 
+        </Routes>
+      </>
   )
 }
 
